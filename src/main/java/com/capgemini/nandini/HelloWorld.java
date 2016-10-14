1f040986756021125a760f1a53c9026fe935abdf
Pritam dash <pdash44@gmail.com>
2016-10-14 18:00:32 +0530

    commited

---
+++ src/main/java/com/capgemini/nandini/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.capgemini.nandini;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Training is good");
+	}
+
+}
